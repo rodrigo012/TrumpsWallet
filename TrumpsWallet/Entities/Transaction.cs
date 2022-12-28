0c6ff3a35c11aa95e2da0ae48847cc58cb3f379e
rodrigo012 <rodrigo.ball@LIVE.COM.AR>
2022-12-28 19:25:58 -0300

    Crear entidad y migración de Transactions

---
+++ TrumpsWallet/Entities/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrumpsWallet.Entities { 
+    public class Transaction : BaseEntity
+{
+    public decimal Amount { get; set; }
+    public string Concept { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+
+    public int to_AccountID { get; set; }
+
+    [ForeignKey("Account")]
+    public int AccountID { get; set; }
+
+    public Account Account { get; set; }
+    [ForeignKey("User")]
+    public int UserID { get; set; }
+
+        //  public User User { get; set; }
+
+        //public to_account_id UserID { get; set;}
+
+
+
+    }
+}
