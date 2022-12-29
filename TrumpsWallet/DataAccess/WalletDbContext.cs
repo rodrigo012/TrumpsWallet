435cc5bb707adeecb9c9b52a6570bf8cef4ed3aa
Franco44305 <Franco44305@gmail.com>
2022-12-29 13:48:02 -0300

    Cargar tablas de Usuarios

--- TrumpsWallet/DataAccess/WalletDbContext.cs
+++ TrumpsWallet/DataAccess/WalletDbContext.cs
@@ -21,18 +21,71 @@ namespace TrumpsWallet.DataAccess
 
         }
 
+        private void SeedDataUsers(ModelBuilder builder)
+        {
+            builder.Entity<User>().HasData(
+                new User()
+                {
+                    FirstName = "Franco",
+                    LastName = "Villarreal",
+                    Email = "Franco44305@gmail.com",
+                    Password = "123456789",
+                    Point = 7,
+                    RoleId = 1
+                },
+                new User()
+                {
+                    
+                    FirstName = "Yelfran",
+                    LastName = "Giuseppe",
+                    Email = "Yelfran@gmail.com",
+                    Password = "Lion222",
+                    Point = 5,
+                    RoleId = 2
+                },
+                new User()
+                {
+                    
+                    FirstName = "Rodrigo",
+                    LastName = "Roman",
+                    Email = "RodrigoRoman@gmail.com",
+                    Password = "LeoMessi2022",
+                    Point = 4,
+                    RoleId = 1
+                },
+                new User()
+                {
+                    
+                    FirstName = "Luciano",
+                    LastName = "Manzanelli",
+                    Email = "ManzanelliLuciano@gmail.com",
+                    Password = "LM1830",
+                    Point = 6,
+                    RoleId = 2
+                },
+                new User()
+                {
+                    
+                    FirstName = "Daniel",
+                    LastName = "Depablos",
+                    Email = "DaniDepablos@gmail.com",
+                    Password = "Mango207",
+                    Point = 3,
+                    RoleId = 1
+                });
+        }
         private void SeedDataRole(ModelBuilder builder)
         {
             builder.Entity<Role>().HasData(
                 new Role()
                 {
-                    Id = 1,
+                   
                     Name = "Admin",
                     Description = "Usuario Administrador",
                 },
                 new Role()
                 {
-                    Id = 2,
+                   
                     Name = "Cliente",
                     Description = "Usuario Cliente",
                 });
@@ -43,7 +96,7 @@ namespace TrumpsWallet.DataAccess
             builder.Entity<Account>().HasData(
                 new Account()
                 {
-                    Id = 1,
+                   
                     creationDate = DateTime.Now,
                     money = 81000,
                     isBlocked = false,
@@ -51,7 +104,7 @@ namespace TrumpsWallet.DataAccess
                 },
                 new Account()
                 {
-                    Id = 2,
+                    
                     creationDate = DateTime.Now,
                     money = 30000,
                     isBlocked = true,
@@ -59,7 +112,7 @@ namespace TrumpsWallet.DataAccess
                 },
                 new Account()
                 {
-                    Id = 3,
+                    
                     creationDate = DateTime.Now,
                     money = 30000,
                     isBlocked = true,
@@ -67,7 +120,7 @@ namespace TrumpsWallet.DataAccess
                 },
                 new Account()
                 {
-                    Id = 4,
+                   
                     creationDate = DateTime.Now,
                     money = 30000,
                     isBlocked = true,
@@ -75,7 +128,7 @@ namespace TrumpsWallet.DataAccess
                 },
                 new Account()
                 {
-                    Id = 5,
+                    
                     creationDate = DateTime.Now,
                     money = 15000,
                     isBlocked = false,
@@ -88,45 +141,44 @@ namespace TrumpsWallet.DataAccess
             builder.Entity<Transaction>().HasData(
                 new Transaction()
                 {
-                    Id = 1,
+                    
                     UserID = 1,
                     AccountID = 1,
                     Date = DateTime.Now,
                     Amount = 2000,
                     Concept = "Transferencia",
                     Type = "Payment",
-                    toAccountID = 2,
+                    toAccountID = 10,
 
                 },
                 new Transaction()
                 {
-                    Id = 2,
+                   
                     UserID = 2,
                     AccountID = 2,
                     Date = DateTime.Now,
                     Amount = 200,
                     Concept = "Transferencia",
                     Type = "Payment",
-                    toAccountID = 5,
+                    toAccountID = 3,
 
                 },
                 new Transaction()
                 {
-                    Id = 3,
-                    UserID = 3,
-                    AccountID = 3,
+                    
+                    UserID = 1,
+                    AccountID = 1,
                     Date = DateTime.Now,
                     Amount = 150,
                     Concept = "Recarga",
-                    Type = "Topup",
-                    toAccountID = 2,
+                    Type = "Topup"
 
                 },
                 new Transaction()
                 {
-                    Id = 4,
-                    UserID = 4,
-                    AccountID = 4,
+                    
+                    UserID = 3,
+                    AccountID = 3,
                     Date = DateTime.Now,
                     Amount = 2000,
                     Concept = "Transferencia",
@@ -136,18 +188,17 @@ namespace TrumpsWallet.DataAccess
                 },
                 new Transaction()
                 {
-                    Id = 5,
-                    UserID = 5,
-                    AccountID = 5,
+                   
+                    UserID = 4,
+                    AccountID = 4,
                     Date = DateTime.Now,
                     Amount = 2000,
                     Concept = "Recarga",
-                    Type = "Topup",
-                    toAccountID = 3,
+                    Type = "Topup"
 
                 });
-
         }
+ 
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<User> Users { get; set; }
         public virtual DbSet<Account> Accounts { get; set; }
