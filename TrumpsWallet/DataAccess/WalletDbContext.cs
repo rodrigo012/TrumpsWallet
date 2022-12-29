382cdbc2d933b6a333187229053437e82bae1e46
yelgom90 <yelfrangomez@gmail.com>
2022-12-28 22:39:40 -0300

    Creación de Model Seed Data de Accounts

--- TrumpsWallet/DataAccess/WalletDbContext.cs
+++ TrumpsWallet/DataAccess/WalletDbContext.cs
@@ -12,9 +12,43 @@ namespace TrumpsWallet.DataAccess
         {
 
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        { 
+            base.OnModelCreating(modelBuilder);
+            this.SeedDataAccounts(modelBuilder);
+        
+        }
 
-        public DbSet<Account> Accounts { get; set; }
+        private void SeedDataAccounts(ModelBuilder builder)
+        {
+            builder.Entity<Account>().HasData(
+                new Account()
+                {
+                    Id = 1,
+                    creationDate = DateTime.Now,
+                    money = 81000,
+                    isBlocked = false,
+                    userId = 1
+                },
+                new Account()
+                {
+                    Id = 2,
+                    creationDate = DateTime.Now,
+                    money = 30000,
+                    isBlocked = true,
+                    userId = 2
+                },
+                new Account()
+                {
+                    Id = 3,
+                    creationDate = DateTime.Now,
+                    money = 1500,
+                    isBlocked = false,
+                    userId = 3
+                });
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
     }
 }
--- TrumpsWallet/Migrations/20221229013430_AccountCreate.Designer.cs
+++ TrumpsWallet/Migrations/20221229013430_AccountCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrumpsWallet.DataAccess;
 
@@ -11,9 +12,10 @@ using TrumpsWallet.DataAccess;
 namespace TrumpsWallet.Migrations
 {
     [DbContext(typeof(WalletDbContext))]
-    partial class WalletDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221229013430_AccountCreate")]
+    partial class AccountCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
