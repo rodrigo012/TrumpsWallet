dded71693362070ab63c461ebcae1b05c8ee1b04
yelgom90 <yelfrangomez@gmail.com>
2023-01-02 00:58:34 -0300

    Agregado de Interface de Role y Transaccion en capa service

---
+++ TrumpsWallet/Core/Services/Intefaces/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using TrumpsWallet.Entities;
+
+namespace TrumpsWallet.Core.Services.Interfaces
+{
+    public interface IRoleService
+    {
+        Task<List<Role>> GetAllRoles();
+
+        Task<Role> GetById(int id);
+        Task<Role> InsertAsync(Role role);
+        Task UpdateRole(int id, Role role);
+        Task DeleteById(int id);
+    }
+}
--- TrumpsWallet/Program.cs
+++ TrumpsWallet/Program.cs
@@ -26,7 +26,6 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<ITransactionService, TransactionService>();
 builder.Services.AddScoped<IRoleService, RoleService>();
 
